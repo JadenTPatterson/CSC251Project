b3fc5dcd0eccce792d595226c5f89405f09b033c
Jaden Patterson <jpatte66@email.cpcc.edu>
2023-10-30 16:52:36 -0400

    Update PolicyHolder.java
    Imported fields and methods from Policy class

---
+++ PolicyHolder.java
@@ -0,0 +1,173 @@
+public class PolicyHolder
+{
+    //getters//
+   /**
+   @return The Policy Number
+   */
+   public String getPolicyNumber()
+   {
+      return policyNumber;
+   }
+   
+   /**
+   @return The Policy Provider's Name
+   */
+   public String getProviderName()
+   {
+      return providerName;
+   }
+   
+   /**
+   @return The Policyholder's first name
+   */
+   public String getFirstName()
+   {
+      return FirstName;
+   }
+   
+   /**
+   @return The Policyholder's last name
+   */
+   public String getLastName()
+   {
+      return LastName;
+   }
+   
+   /**
+   @return The Policyholder's age
+   */
+   public int getAge()
+   {
+      return age;
+   }
+   
+   /**
+   @return The Policyholder's smoking status
+   */
+   public String getSmokingStatus()
+   {
+      return smokingStatus;
+   }
+   
+   /**
+   @return The Policyholder's height
+   */
+   public double getHeight()
+   {
+      return height;
+   }
+   
+   /**
+   @return The Policyholder's weight
+   */
+   public double getWeight()
+   {
+      return weight;
+   }
+   
+   //setters//
+   
+   /**
+   @param pNumber The Policy Number
+   */
+   public void setPolicyNumber(String pNumber)
+   {
+      policyNumber = pNumber;
+   }
+   
+   /**
+   @param pName The Policy Provider's name
+   */
+   public void setProviderName(String pName)
+   {
+      providerName = pName;
+   }
+   
+   /**
+   @param fName The Policy Provider's first name
+   */
+   public void setFirstName(String fName)
+   {
+      FirstName = fName;
+   }
+   
+   /**
+   @param lName The Policy Provider's last name
+   */
+   public void setLastName(String lName)
+   {
+      LastName = lName;
+   }
+   
+   /**
+   @param a The Policy Provider's age
+   */
+   public void setAge(int a)
+   {
+      age = a;
+   }
+   
+   /**
+   @param sStatus The Policy Provider's smoking status
+   */
+   public void setSmokingStatus(String sStatus)
+   {
+      smokingStatus = sStatus;
+   }
+   
+   /**
+   @param h The Policy Provider's height
+   */
+   public void setHeight(double h)
+   {
+      height = h;
+   }
+   
+   /**
+   @param w The Policy Provider's weight
+   */
+   public void setWeight(double w)
+   {
+      weight = w;
+   }
+   
+   /**
+   Calculates the Policyholder's BMI
+   @return The BMI of the Policyholder
+   */
+   public double getBMI()
+   {
+      final double CONVFACTOR = 703;
+      
+      return (weight * CONVFACTOR) / (height * height);
+   }
+   
+   /**
+   Calculates the Policy's price
+   @return The price of the Policy
+   */
+   public double getPrice()
+   {
+      final double BASE_PRICE = 600;
+      final double ADDITIONAL_FEE_AGE = 75;
+      final double ADDITIONAL_FEE_SMOKING = 100;
+      final double ADDITIONAL_FEE_PER_BMI = 20;
+      
+      final int AGE_THRESHOLD = 50;
+      final int BMI_THRESHOLD = 35;
+      
+      double price = BASE_PRICE;
+      
+      if(age > AGE_THRESHOLD)
+         price += ADDITIONAL_FEE_AGE;
+         
+      if(smokingStatus.equalsIgnoreCase("smoker"))
+         price += ADDITIONAL_FEE_SMOKING;
+      
+      if(getBMI() > BMI_THRESHOLD)
+         price += ((getBMI() - BMI_THRESHOLD) * ADDITIONAL_FEE_PER_BMI);
+         
+      return price;
+   }
+
+}
